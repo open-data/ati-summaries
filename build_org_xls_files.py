db37ac195a713cf397b0eaa48f18c08910a2573c
Ian Ward <ian@excess.org>
2013-09-05 12:27:30 -0400

    split reading and fixing csv into separate module

--- build_org_xls_files.py
+++ build_org_xls_files.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-import re
 import csv
-import itertools
 import json
 
+from read_fix_csv import unicode_csv_reader, parse_source
 from write_xls import write_matched
 
 ENG_SOURCE = 'data/ati_en.csv'
@@ -13,79 +11,8 @@ FRA_SOURCE = 'data/ati_fr.csv'
 ORG_MAPPING = 'data/orgMapping.csv'
 DATA_GC_CA_ORGS = 'data/data_gc_ca_orgs.json'
 
-ORG_FIXUPS = {
-    u'http://www.cbc.radio-canada.ca/_structures/default/_img/logo-large.png':
-        u'Canadian Broadcasting Corportation',
-    u'Port of Trois-Rivi�res': u'Port of Trois-Rivières',
-    u'Élections Canada':
-        u'Bureau du directeur général des élections (Élections Canada)',
-    }
-
 DUPLICATED = 'duplicated'
 
-def unicode_csv_reader(src, dialect=csv.excel, **kwargs):
-    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
-    csv_reader = csv.reader(open(src, 'rb'),
-                            dialect=dialect, **kwargs)
-    for row in csv_reader:
-        # decode UTF-8 back to Unicode, cell by cell:
-        yield [unicode(cell, 'utf-8') for cell in row]
-
-def fix_org_name(name):
-    name = name.replace(u'  ', u' ')
-    name = ORG_FIXUPS.get(name, name)
-    return name
-
-def fix_num_pages(pages):
-    """
-    Some page numbers were entered with a "." thousands separator
-    which was then interpreted as a floating point by Excel.
-
-    Try to repair that damage.
-    """
-    if u'.' in pages:
-        try:
-            return unicode(int(float(pages) * 1000))
-        except ValueError:
-            pass
-    return pages
-
-def normalize_request_number(num):
-    """
-    Some input data includes request numbers entered with slight
-    variations such as: A2011-00013, A-2011-00013 and
-    A-2013-00001, A-2013-0001
-
-    This function normalizes request numbers by converting digits
-    to integers and removing non-word characters such as '-'
-    """
-
-    split_re = re.compile(
-        ur'[^\W\d]+' # "not (non-alphanumeric or numbers)" ~= only letters
-        ur'|\d+' # also group numbers together
-        , re.UNICODE)
-    return tuple(
-        int(g) if isdigit else g
-        for isdigit, group in itertools.groupby(
-            split_re.findall(num), key=lambda x: x.isdigit())
-        for g in group
-        )
-
-def parse_source(src):
-    for num, row in enumerate(unicode_csv_reader(src, delimiter="|")):
-        assert len(row) == 10, (src, num, len(row))
-        yield {
-            'id': row[0],
-            'org': fix_org_name(row[1]),
-            'year': row[2],
-            'month': row[3],
-            'num': row[4],
-            'norm_num': normalize_request_number(row[4]),
-            'summary': row[5],
-            'disp': row[6],
-            'pages': fix_num_pages(row[7]),
-            'contact': row[8],
-            }
 
 def group_requests_by_org(source):
     org_numbers = {}
---
+++ read_fix_csv.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+import re
+import csv
+import itertools
+
+ORG_FIXUPS = {
+    u'http://www.cbc.radio-canada.ca/_structures/default/_img/logo-large.png':
+        u'Canadian Broadcasting Corportation',
+    u'Port of Trois-Rivi�res': u'Port of Trois-Rivières',
+    u'Élections Canada':
+        u'Bureau du directeur général des élections (Élections Canada)',
+    }
+
+def unicode_csv_reader(src, dialect=csv.excel, **kwargs):
+    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
+    csv_reader = csv.reader(open(src, 'rb'),
+                            dialect=dialect, **kwargs)
+    for row in csv_reader:
+        # decode UTF-8 back to Unicode, cell by cell:
+        yield [unicode(cell, 'utf-8') for cell in row]
+
+def fix_org_name(name):
+    name = name.replace(u'  ', u' ')
+    name = ORG_FIXUPS.get(name, name)
+    return name
+
+def fix_num_pages(pages):
+    """
+    Some page numbers were entered with a "." thousands separator
+    which was then interpreted as a floating point by Excel.
+
+    Try to repair that damage.
+    """
+    if u'.' in pages:
+        try:
+            return unicode(int(float(pages) * 1000))
+        except ValueError:
+            pass
+    return pages
+
+def normalize_request_number(num):
+    """
+    Some input data includes request numbers entered with slight
+    variations such as: A2011-00013, A-2011-00013 and
+    A-2013-00001, A-2013-0001
+
+    This function normalizes request numbers by converting digits
+    to integers and removing non-word characters such as '-'
+    """
+
+    split_re = re.compile(
+        ur'[^\W\d]+' # "not (non-alphanumeric or numbers)" ~= only letters
+        ur'|\d+' # also group numbers together
+        , re.UNICODE)
+    return tuple(
+        int(g) if isdigit else g
+        for isdigit, group in itertools.groupby(
+            split_re.findall(num), key=lambda x: x.isdigit())
+        for g in group
+        )
+
+def parse_source(src):
+    for num, row in enumerate(unicode_csv_reader(src, delimiter="|")):
+        assert len(row) == 10, (src, num, len(row))
+        yield {
+            'id': row[0],
+            'org': fix_org_name(row[1]),
+            'year': row[2],
+            'month': row[3],
+            'num': row[4],
+            'norm_num': normalize_request_number(row[4]),
+            'summary': row[5],
+            'disp': row[6],
+            'pages': fix_num_pages(row[7]),
+            'contact': row[8],
+            }
