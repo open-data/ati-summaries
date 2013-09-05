66579e0384d53f5d4cbfdd2daeae2203fe0fcabc
Ian Ward <ian@excess.org>
2013-09-05 12:18:47 -0400

    split excel writing into separate module

--- build_org_xls_files.py
+++ build_org_xls_files.py
@@ -4,12 +4,14 @@
 import re
 import csv
 import itertools
+import json
 
-import xlwt
+from write_xls import write_matched
 
 ENG_SOURCE = 'data/ati_en.csv'
 FRA_SOURCE = 'data/ati_fr.csv'
 ORG_MAPPING = 'data/orgMapping.csv'
+DATA_GC_CA_ORGS = 'data/data_gc_ca_orgs.json'
 
 ORG_FIXUPS = {
     u'http://www.cbc.radio-canada.ca/_structures/default/_img/logo-large.png':
@@ -126,44 +128,6 @@ def write_unmatched(org_reqs, unmatched, file_name, org_mapping):
         for org in unmatched)
 
 
-def write_matched(req_pairs, eng_org, fra_org, file_name):
-    book = xlwt.Workbook()
-    sheet = book.add_sheet('ATI AI')
-    sheet.write(0, 3, eng_org)
-    sheet.write(0, 4, fra_org)
-    for col, h in enumerate([u'Year / Annee', u'Month / Mois',
-            u'Request Number / Numero de la demande',
-            u'ENG Summary / ENG Sommaire de la demande',
-            u'FRA Summary / FRA Sommaire de la demande',
-            u'Disposition',
-            u'Number of Pages / Nombre de pages', 
-            u'Contact Information / Information de contact']):
-        sheet.write(1, col, h)
-    sheet.col(0).width = 5 * 256
-    sheet.col(1).width = 4 * 256
-    sheet.col(2).width = 15 * 256
-    sheet.col(3).width = 30 * 256
-    sheet.col(4).width = 30 * 256
-    sheet.col(5).width = 40 * 256
-
-    row = 2
-    sheet.set_panes_frozen(True) # frozen headings instead of split panes
-    sheet.set_horz_split_pos(row) # in general, freeze after last heading row
-    sheet.set_remove_splits(True) # if user does unfreeze, don't leave a split there
-
-    req_pairs = sorted(req_pairs, key=lambda (eng, fra):
-        (eng['year'], eng['month'], eng['num']))
-    for eng, fra in req_pairs:
-        def c(key):
-            if eng[key] != fra[key]:
-                return eng[key] + u' / ' + fra[key]
-            return eng[key]
-        for col, t in enumerate([c('year'), c('month'),
-                c('num'), eng['summary'], fra['summary'],
-                c('disp'), c('pages'), c('contact')]):
-            sheet.write(row, col, t)
-        row += 1
-    book.save(file_name)
 
 
 def main():
---
+++ write_xls.py
@@ -0,0 +1,40 @@
+import xlwt
+
+def write_matched(req_pairs, eng_org, fra_org, file_name):
+    book = xlwt.Workbook()
+    sheet = book.add_sheet('ATI AI')
+    sheet.write(0, 3, eng_org)
+    sheet.write(0, 4, fra_org)
+    for col, h in enumerate([u'Year / Annee', u'Month / Mois',
+            u'Request Number / Numero de la demande',
+            u'ENG Summary / ENG Sommaire de la demande',
+            u'FRA Summary / FRA Sommaire de la demande',
+            u'Disposition',
+            u'Number of Pages / Nombre de pages', 
+            u'Contact Information / Information de contact']):
+        sheet.write(1, col, h)
+    sheet.col(0).width = 5 * 256
+    sheet.col(1).width = 4 * 256
+    sheet.col(2).width = 15 * 256
+    sheet.col(3).width = 30 * 256
+    sheet.col(4).width = 30 * 256
+    sheet.col(5).width = 40 * 256
+
+    row = 2
+    sheet.set_panes_frozen(True) # frozen headings instead of split panes
+    sheet.set_horz_split_pos(row) # in general, freeze after last heading row
+    sheet.set_remove_splits(True) # if user does unfreeze, don't leave a split there
+
+    req_pairs = sorted(req_pairs, key=lambda (eng, fra):
+        (eng['year'], eng['month'], eng['num']))
+    for eng, fra in req_pairs:
+        def c(key):
+            if eng[key] != fra[key]:
+                return eng[key] + u' / ' + fra[key]
+            return eng[key]
+        for col, t in enumerate([c('year'), c('month'),
+                c('num'), eng['summary'], fra['summary'],
+                c('disp'), c('pages'), c('contact')]):
+            sheet.write(row, col, t)
+        row += 1
+    book.save(file_name)
