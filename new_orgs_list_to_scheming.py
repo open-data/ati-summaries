2877dfd98eefa0609c22f0b88933b764da226354
Ian Ward <ian@excess.org>
2013-12-02 17:18:52 -0500

    merge data.gc.ca uuids into scheming jsonl

--- new_orgs_list_to_scheming.py
+++ new_orgs_list_to_scheming.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import json
+import uuid
 from read_fix_csv import unicode_csv_reader
 
 """
@@ -9,6 +10,7 @@ imported by ckanext-scheming as organizations
 """
 
 SOURCE_ORGS = 'data/orgs-utf8.csv'
+ORG_UUIDS = 'data/data_gc_ca_orgs.json'
 OUTPUT_JSONL = 'data/orgs.jsonl'
 
 def org_name(en_abbr, fr_abbr, dept_no):
@@ -21,13 +23,25 @@ def org_name(en_abbr, fr_abbr, dept_no):
 def main():
     orgs = unicode_csv_reader(SOURCE_ORGS)
     out = open(OUTPUT_JSONL, 'w')
+    org_uuids = json.loads(open(ORG_UUIDS).read())
     for o in orgs:
         if o[0] == 'English':
             break
     for o in orgs:
+        old_org = org_uuids.get(o[8])
+        name = org_name(o[1], o[5], o[8])
+        assert name
+        if old_org:
+            oid = old_org['uuid']
+        else:
+            print 'no uuid:', o[0], repr(o[8])
+            oid = str(uuid.uuid5(uuid.NAMESPACE_URL,
+                (u'http://data.gc.ca/data/organization/%s' % name).encode(
+                    'ascii')))
         out.write(json.dumps({
+            'id': oid,
             'title': o[0],
-            'name': org_name(o[1], o[5], o[8]),
+            'name': name,
             'alternate_names': o[2],
             'homepage': o[3],
             'title_fr': o[4],
