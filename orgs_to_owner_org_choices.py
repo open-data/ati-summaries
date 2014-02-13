d1945f6c908e3362e6c412a6447cf31462615c15
Ian Ward <ian@excess.org>
2014-02-13 16:30:27 -0500

    match owner org choices better

--- orgs_to_owner_org_choices.py
+++ orgs_to_owner_org_choices.py
@@ -9,17 +9,18 @@ def main():
     choices = []
     for line in open(ORGS):
         org = json.loads(line)
-        if 'department_number' not in org:
+        if not org.get('department_number'):
             continue
         choices.append({
             'eng': org['title'],
             'fra': org['title_fr'],
-            'id': org['department_number'],
+            'id': int(org['department_number']),
             'key': org['name'],
             })
-    out = json.dumps(choices, ensure_ascii=False, indent=2, sort_keys=True)
-    for line in out.split('\n'):
-        print '          ' + line
+    out = json.dumps(choices, indent=2, sort_keys=True,
+        separators=(', ', ': '))
+    for line in out.split('\n')[1:-1]:
+        print '          ' + line.encode('utf-8')
 
 if __name__ == '__main__':
     main()
