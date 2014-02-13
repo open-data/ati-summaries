b0b168e807a582815a3ea9b38e25ff890f34229a
Ian Ward <ian@excess.org>
2014-02-13 17:01:33 -0500

    org migrate to new list

---
+++ create_org_migrate_csv.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import json
+
+ORGS = 'data/orgs.jsonl'
+DATA_GC_CA_ORGS = 'data/data_gc_ca_orgs.json'
+
+def main():
+    migrate = []
+    portal_orgs = json.load(open(DATA_GC_CA_ORGS))
+    for line in open(ORGS):
+        org = json.loads(line)
+        dept_id = org.get('department_number')
+        if not dept_id:
+            continue
+        original_name = pilot_uuid = ''
+        if dept_id in portal_orgs:
+            po = portal_orgs.pop(dept_id)
+            original_name = po['name']
+            if po['uuid'] == po['uuid'].upper():
+                # dirty hack: portal uuids are all uppercase
+                pilot_uuid = po['uuid']
+        migrate.append((
+            org['name'],
+            org['title'],
+            org['alternate_names'] if org['alternate_names'] != org['title'] else '',
+            original_name,
+            pilot_uuid,
+            ))
+
+    migrate.sort()
+    for line in migrate:
+        print ','.join('"%s"' % v.encode('utf-8') for v in line)
+
+
+if __name__ == '__main__':
+    main()
