2abccf93ca28f490378d7b20ed65f31cf12e8b27
Ian Ward <ian@excess.org>
2014-02-20 14:37:43 -0500

    generate transitional_orgs.jsonl from orgs.jsonl

---
+++ transitional_orgs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import json
+
+INPUT_JSONL = 'data/orgs.jsonl'
+
+for l in open(INPUT_JSONL):
+    org = json.loads(l)
+    print json.dumps({
+        'name': org['name'],
+        'title' : u'%s | %s' % (org['title'], org['title_fr']),
+        'extras': [
+            {'key': 'department_number', 'value': org['department_number']},
+            {'key': 'shortform', 'value': org['shortform']},
+            {'key': 'shortform_fr', 'value': org['shortform_fr']},
+            ],
+        })
